0d06e0ed4ea034854e8e0fe84ab20aab183f10b8
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2023-01-25 18:07:44 -0500

    add pathplanner utils back in

--- src/main/kotlin/org/team4099/lib/pathfollow/TrajectoryUtils.kt
+++ src/main/kotlin/org/team4099/lib/pathfollow/TrajectoryUtils.kt
@@ -1,5 +1,6 @@
 package org.team4099.lib.pathfollow
 
+import com.pathplanner.lib.PathPlannerTrajectory
 import edu.wpi.first.math.trajectory.TrajectoryParameterizer
 import org.team4099.lib.geometry.Pose2d
 import org.team4099.lib.geometry.Translation2d
@@ -7,6 +8,7 @@ import org.team4099.lib.units.LinearVelocity
 import org.team4099.lib.units.base.meters
 import org.team4099.lib.units.base.seconds
 import org.team4099.lib.units.derived.angle
+import org.team4099.lib.units.derived.radians
 import org.team4099.lib.units.inMetersPerSecond
 import org.team4099.lib.units.inMetersPerSecondPerSecond
 import org.team4099.lib.units.perSecond
@@ -68,4 +70,20 @@ fun trajectoryFromPath(
     }
 
   return Trajectory(states)
+}
+
+fun trajectoryFromPathPlanner(pathPlannerTrajectory: PathPlannerTrajectory): Trajectory {
+  return Trajectory(
+    pathPlannerTrajectory.states.map { state ->
+      state as PathPlannerTrajectory.PathPlannerState
+      TrajectoryState(
+        state.timeSeconds.seconds,
+        Pose2d(Translation2d(state.poseMeters.translation), state.holonomicRotation.angle),
+        state.poseMeters.rotation.angle,
+        state.velocityMetersPerSecond.meters.perSecond,
+        state.accelerationMetersPerSecondSq.meters.perSecond.perSecond,
+        angularVelocity = state.angularVelocityRadPerSec.radians.perSecond
+      )
+    }
+  )
 }
